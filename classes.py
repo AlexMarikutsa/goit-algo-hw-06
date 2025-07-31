88ebe2dec5c053e7419dc54acf2f1de856817447
AlexMarikutsa <a.marikutsa@ukr.net>
2025-07-31 14:01:46 +0300

    Initial commit

---
+++ classes.py
@@ -0,0 +1,59 @@
+from collections import UserDict
+from dataclasses import dataclass
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+@dataclass
+class Name:
+    value: Field
+
+@dataclass
+class Phone:
+    value: Field
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+    
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        if not self.phones:
+            return ValueError("No phones to edit.")
+        for p in self.phones:
+            if p.value == old_phone:
+                p.value = new_phone
+                break
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p.value
+        return None
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)    
+    
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+    def __str__(self):
+        return "\n".join(str(record) for record in self.data.values())
